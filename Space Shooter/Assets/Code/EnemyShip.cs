25f30f85021a7b761a314ec4ac2eb43637df9ae0
WUStudent <wustudent@webster.edu>
2023-03-22 12:34:02 -0500

    initial commit

---
+++ Space Shooter/Assets/Code/EnemyShip.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : Ship
+{
+    Transform target;
+    public bool canFireAtPlayer;
+
+    void Start()
+    {
+        target = FindObjectOfType<PlayerShip>().transform;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerShip>())
+        {
+            collision.gameObject.GetComponent<PlayerShip>().TakeDamage(1);
+            Explode();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlyTowardPlayer();
+
+        if (canFireAtPlayer && canShoot)
+        {
+            FireProjectile();
+        }
+    }
+
+    void FlyTowardPlayer()
+    {
+        Vector2 directionToFace = new Vector2(
+            target.position.x - transform.position.x, target.position.y - transform.position.y);
+        transform.up = directionToFace;
+        Thrust();
+    }
+}
---
+++ Space Shooter/Assets/Code/Ship.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public GameObject projectilePrefab;
+    public Transform projectileSpawnPoint;
+
+    public float acceleration;
+    public float maxSpeed;
+    public int maxArmor;
+    public float fireRate;
+    public float projectileSpeed;
+
+    [HideInInspector] public float currentSpeed;
+    [HideInInspector] public int currentArmor;
+
+    [HideInInspector] public bool canShoot;
+
+    [HideInInspector] ParticleSystem thrustParticles;
+    private void Awake()
+    {
+        currentArmor = maxArmor;
+        canShoot = true;
+        thrustParticles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (rigidbody2D.velocity.magnitude > maxSpeed)
+        {
+            rigidbody2D.velocity = rigidbody2D.velocity.normalized * maxSpeed;
+        }
+    }
+
+    public void Thrust()
+    {
+        rigidbody2D.AddForce(transform.up * acceleration);
+        thrustParticles.Emit(1);
+    }
+    public void FireProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
+        projectile.GetComponent<Rigidbody2D>().AddForce(transform.up * projectileSpeed);
+        projectile.GetComponent<Projectile>().GetFired(gameObject);
+        Destroy(projectile, 4);
+        StartCoroutine(FireRateBuffer());
+    }
+
+    private IEnumerator FireRateBuffer()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(fireRate); 
+        canShoot = true;
+    }
+
+    public void TakeDamage(int damageToGive)
+    {
+        //TODO: play getHitSound
+        currentArmor -= damageToGive;
+        if (currentArmor <= 0)
+        {
+            Explode();
+        }
+
+        if (GetComponent<PlayerShip>())
+        {
+            HUD.Instance.DisplayHealth(currentArmor, maxArmor);
+        }
+    }
+    public void Explode()
+    {
+        ScreenShakeManager.Instance.ShakeScreen();
+        Instantiate(Resources.Load("Explosion"), transform.position, transform.rotation);
+        Destroy(gameObject);
+
+        FindObjectOfType<EnemyShipSpawner>().CountEnemyShips();
+    }
+}
