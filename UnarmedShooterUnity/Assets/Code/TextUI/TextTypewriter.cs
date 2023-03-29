da30706f7d89a3e8afdced49f62eff782257ce67
WUStudent <wustudent@webster.edu>
2023-03-29 15:27:09 -0500

    Textbox complete

--- UnarmedShooterUnity/Assets/Code/TextUI/TextTypewriter.cs
+++ UnarmedShooterUnity/Assets/Code/TextUI/TextTypewriter.cs
@@ -2,36 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using TMPro;
 
 public class TextTypewriter : MonoBehaviour
 {
 
     TextMeshProUGUI textbox;
+    Image textbox_IMG;
+    Image sprite_IMG;
 
     int visibleCharacters = 0;
     int currentDialogueNumber = 0;
-    bool isDoneTyping = true;
     bool inDialogue = false;
 
     TextData thisDialogueList;
+    public static TextTypewriter instance;
+
+    public UnityEvent m_StartedDialogue;
+    public UnityEvent m_EndedDialogue;
 
     [SerializeField] int charactersPerFrame = 2;
+
 
     private void Awake()
     {
         textbox = GetComponent<TextMeshProUGUI>();
+        textbox_IMG = GetComponentInParent<Image>();
+        sprite_IMG = GetComponentInChildren<Image>();
+
+        
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        HideText();
     }
 
-    //TODO: Add StartDialogue trigger
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (inDialogue)
+            {
+                NextDialogue();
+            }
+        }
+    }
+
+    private void HideText()
+    {
+        textbox_IMG.enabled = false;
+        sprite_IMG.enabled = false;
+        textbox.text = "";
+    }
+
+    private void ShowText()
+    {
+        textbox_IMG.enabled = true;
+    }
+
+    private void ShowSprite(Sprite sprite = null)
+    {
+        if (sprite != null)
+        {
+            sprite_IMG.enabled = true;
+            sprite_IMG.sprite = sprite;
+        }
+        else
+        {
+            sprite_IMG.enabled = false;
+        }
+    }
+
     //Starts dialogue sequence if not in dialogue already
     public void StartDialogue(TextData dialogueList)
     {
         if (!inDialogue)
         {
+            m_StartedDialogue.Invoke();
+            ShowText();
+            
             inDialogue = true;
             currentDialogueNumber = 0;
             thisDialogueList = dialogueList;
+
+            ShowSprite(dialogueList.dialogue[0].image);
+
             StartCoroutine("Typewrite", dialogueList.dialogue[0].dialogueText);
         } 
     }
@@ -39,34 +97,30 @@ public class TextTypewriter : MonoBehaviour
     //Triggers next dialogue in the sequence
     //If not done typing the current text, finish it immedietely
     //If done with the whole sequence, end the dialogue
-    //TODO: Add EndDialogue trigger
     public void NextDialogue()
     {
         currentDialogueNumber++;
 
         if (currentDialogueNumber < thisDialogueList.dialogue.Length)
         {
-            if (!isDoneTyping)
-            {
-                StopAllCoroutines();
-                textbox.maxVisibleCharacters = textbox.text.Length;
-                isDoneTyping = true;
-            }
-            else
-            {
-                StartCoroutine("Typewrite", thisDialogueList.dialogue[currentDialogueNumber].dialogueText);
-            }
+
+            StartCoroutine("Typewrite", thisDialogueList.dialogue[currentDialogueNumber].dialogueText);
+
+            ShowSprite(thisDialogueList.dialogue[currentDialogueNumber].image);
         }
         else
         {
+            
             EndDialogue();
         }
     }
 
     public void EndDialogue()
     {
-        isDoneTyping = true; //Failsafe
         inDialogue = false;
+
+        HideText();
+        m_EndedDialogue.Invoke();
     }
 
     //Types the text of the dialogue
@@ -75,7 +129,6 @@ public class TextTypewriter : MonoBehaviour
         visibleCharacters = 0;
         textbox.maxVisibleCharacters = 0;
         textbox.text = text;
-        isDoneTyping = false;
 
         for (int i=0; i<=text.Length; i++)
         {
@@ -83,8 +136,7 @@ public class TextTypewriter : MonoBehaviour
             visibleCharacters+=charactersPerFrame;
             yield return new WaitForSeconds(0.01f);
         }
-
-        isDoneTyping = true;
+        
         yield return null;
     }
 }
