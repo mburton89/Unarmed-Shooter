5bee0dc5b568686e3a056ee9ca4f9366bc074acd
WUStudent <wustudent@webster.edu>
2023-05-03 15:17:50 -0500

    DO NOT PULL

--- UnarmedShooterUnity/Assets/Code/Ship.cs
+++ UnarmedShooterUnity/Assets/Code/Ship.cs
@@ -9,6 +9,7 @@ public class Ship : MonoBehaviour
     public Rigidbody2D rb;
     public GameObject projectilePrefab;
     public Transform projectileSpawnPoint;
+    public Vector2 lastVelocity;
 
     public float acceleration;
     public float maxSpeed;
@@ -27,6 +28,12 @@ public class Ship : MonoBehaviour
     [HideInInspector] public int currentArmor;
     [HideInInspector] public int currentHealth;
     [HideInInspector] public bool canShoot;
+    public bool incrementBoost;
+    public bool shieldDeployed;
+    public bool trackVelocity;
+
+    public static GameObject[] powerUpsPrefabs;
+    public float dropChance;
     [HideInInspector] public bool boostUpSent;
 
     [SerializeField] ParticleSystem thrustParticles;
@@ -37,12 +44,16 @@ public class Ship : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
         currentBoostEnergy = maxBoost;
+        incrementBoost = false;
+        shieldDeployed = false;
         boostUpSent = true;
         currentHealth = maxHealth;
 
         if (thrustParticles == null && GetComponent<Dragonfly>() == null) {
             thrustParticles = GetComponentInChildren<ParticleSystem>();
         }
+        powerUpsPrefabs = Resources.LoadAll<GameObject>("PowerUps");
+
     }
 
     private void FixedUpdate()
@@ -114,6 +125,9 @@ public class Ship : MonoBehaviour
         if (GetComponent<PlayerShip>())
         {
             HUD.Instance.DisplayHealth(currentArmor, currentHealth);
+            DoomguyHealthManager.Instance.ShowCorrectHealhPortait(currentArmor, currentHealth);
+            DoomguyHealthManager.Instance.ShowSad();
+            GetComponent<PlayerShip>().TakePlayerDamage();
         }
     }
     public void Explode()
@@ -121,6 +135,15 @@ public class Ship : MonoBehaviour
         ScreenShakeManager.Instance.ShakeScreen();
         Instantiate(Resources.Load("Explosion"), transform.position, transform.rotation);
         Destroy(gameObject);
+
+        if (tag == "Enemy")
+        {
+            if (Random.Range(0f, 1f) <= dropChance)
+            {
+                int rand = Random.Range(0, powerUpsPrefabs.Length);
+                Instantiate(powerUpsPrefabs[rand], transform.position, transform.rotation);
+            }
+        }
 
         FindObjectOfType<EnemyShipSpawner>().CountEnemyShips();
     }
